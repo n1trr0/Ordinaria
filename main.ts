ae3eed2d0631c14c1883f5d9211a33e4d4bdd634
Raul <raulgarcicespedes@gmail.com>
2025-01-28 18:06:16 +0100

    deno deploy

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
-
+//deno add npm:@apolloserver
+//deno add npm:graphql
 const MONGO_URL = Deno.env.get("MONGO_URL");
 if (!MONGO_URL) {throw new Error("Mongo URL not found")};
 
@@ -9,10 +10,29 @@ console.info("Client connected");
 
 const db = Client.db("Ordinaria"); 
 
+//APIREST
+/*
+const handler = async (req: Request): Promise<Response> => {
+  const method = req.method;
+  const url = new URL(req.url);
+  const path = url.pathname;
 
+  if(method === "GET"){
+    
+  }else if(method === "POST"){
 
+  }else if(method === "PUT"){
 
+  }else if(method === "DELETE"){
 
+  }
+  return new Response("Endpoint not found", {status: 404});
+}
+
+Deno.serve({port:3000}, handler);
+*/
+
+//
 
 
 /*
